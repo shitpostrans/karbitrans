97db5e329590bbd2feb809ef3e16a8c7fd778072
Dhika Rizky <deka_r@outlook.com>
2024-11-03 07:39:20 +0700

    feat: make questions multipage

--- src/main.ts
+++ src/main.ts
@@ -8,32 +8,23 @@ interface Stop {
   popularity: number
 }
 
-const MAX_CHECKED_COUNT = 3
+let currentStep = 1
+const usedIndex: Number[] = []
 
 function getRandomInRange(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 const stops: Stop[] = []
-const stopListElement = document.querySelector<HTMLUListElement>('#stop-list')
 const stopListInputTemplate = document.querySelector<HTMLTemplateElement>('#stop-list-item')
 
-function handleStopOnChecked() {
+function handleStopOnChecked(e: Event) {
+  const stopListElement = (e.currentTarget as HTMLElement).closest('.stop-list')
+
   stopListElement!.classList.add('border-transparent')
   stopListElement!.classList.remove('border-red-500')
   stopListElement!.nextElementSibling?.classList.add('hidden')
   stopListElement!.nextElementSibling?.classList.remove('block')
-
-  const checkedLength = stopListElement!.querySelectorAll('input:checked').length
-  if (checkedLength >= MAX_CHECKED_COUNT) {
-    for (const unchecked of stopListElement!.querySelectorAll<HTMLInputElement>('input:not(:checked)')) {
-      unchecked.disabled = true
-    }
-  } else {
-    for (const unchecked of stopListElement!.querySelectorAll<HTMLInputElement>('input:not(:checked)')) {
-      unchecked.disabled = false
-    }
-  }
 }
 
 async function setup() {
@@ -61,8 +52,8 @@ async function setup() {
   }
 }
 
-function loadStops(count = 10) {
-  const usedIndex: Number[] = []
+function getStops(count = 10) {
+  const returningStops: Stop[] = []
 
   for (let i = 0; i < count; i++) {
     let index = getRandomInRange(0, stops.length - 1)
@@ -72,16 +63,11 @@ function loadStops(count = 10) {
 
     const stop = stops[index]
 
-    const cloned = stopListInputTemplate!.content.cloneNode(true) as HTMLElement
-    const checkbox = cloned.querySelector<HTMLInputElement>('.stop-list-checkbox')
-    const label = cloned.querySelector<HTMLInputElement>('.stop-list-label')
-    checkbox!.value = stop.id.toString()
-    checkbox!.addEventListener('change', handleStopOnChecked)
-    label!.textContent = stop.stopName
-
-    stopListElement?.appendChild(cloned)
+    returningStops.push(stop)
     usedIndex.push(index)
   }
+
+  return returningStops
 }
 
 function calculateKarbitScore(name: string, stopIds: number[]) {
@@ -100,7 +86,24 @@ function calculateKarbitScore(name: string, stopIds: number[]) {
 
 async function bootstrap() {
   await setup()
-  loadStops()
+
+  // Load stops for step 2-4
+  for (let i = 2; i < 5; i++) {
+    const stops = getStops(4)
+    const stopListElement = document.querySelector(`.step-${i} .stop-list`)
+
+    for (const stop of stops) {
+      const cloned = stopListInputTemplate!.content.cloneNode(true) as HTMLElement
+      const checkbox = cloned.querySelector<HTMLInputElement>('.stop-list-checkbox')
+      const label = cloned.querySelector<HTMLInputElement>('.stop-list-label')
+      checkbox!.value = stop.id.toString()
+      checkbox!.addEventListener('change', handleStopOnChecked)
+      checkbox!.name = `${checkbox!.name}-step-${i}`
+      label!.textContent = stop.stopName
+
+      stopListElement?.appendChild(cloned)
+    }
+  }
 }
 
 bootstrap()
@@ -109,23 +112,144 @@ document.querySelector('form')?.addEventListener('submit', (e) => {
   e.preventDefault()
   const formData = new FormData(e.currentTarget as HTMLFormElement)
   const name = formData.get('name')
-  const selectedStopIDs = formData.getAll('selectedStops')
+  const selectedStopIDs = Array.from(formData.entries()).filter(([key]) => key.startsWith('selectedStops')).map(([, value]) => Number.parseInt(value.toString()))
+  console.log(selectedStopIDs)
+  const { hash } = calculateKarbitScore(name?.toString() ?? '', selectedStopIDs)
 
-  if (!selectedStopIDs || selectedStopIDs.length === 0 || selectedStopIDs.length !== MAX_CHECKED_COUNT) {
+  window.location.href = `/result?hash=${hash}`
+})
+
+function validateCurrentStep(value: FormDataEntryValue | null) {
+  const selectedStopValue = value
+  const stopListElement = document.querySelector(`.step-${currentStep} .stop-list`)
+  if (!selectedStopValue || !selectedStopValue.toString()) {
     stopListElement?.classList.remove('border-transparent')
     stopListElement?.classList.add('border-red-500')
     stopListElement?.nextElementSibling?.classList.remove('hidden')
     stopListElement?.nextElementSibling?.classList.add('block')
-
-    return
-  } else {
-    stopListElement?.classList.add('border-transparent')
-    stopListElement?.classList.remove('border-red-500')
-    stopListElement?.nextElementSibling?.classList.add('hidden')
-    stopListElement?.nextElementSibling?.classList.remove('block')
+    return false
   }
 
-  const { hash } = calculateKarbitScore(name?.toString() ?? '', selectedStopIDs.map(selectedStopID => Number.parseInt(selectedStopID.toString())))
+  stopListElement?.classList.add('border-transparent')
+  stopListElement?.classList.remove('border-red-500')
+  stopListElement?.nextElementSibling?.classList.add('hidden')
+  stopListElement?.nextElementSibling?.classList.remove('block')
 
-  window.location.href = `/result?hash=${hash}`
+  return true
+}
+
+document.querySelector('#next-button')?.addEventListener('click', (e) => {
+  const nextButton = e.currentTarget as HTMLButtonElement
+  const formData = new FormData(document.querySelector('form') as HTMLFormElement)
+  const name = formData.get('name')
+
+  if (currentStep === 1) {
+    if (!name) {
+      document.querySelector('#name')?.classList.add('border-red-500')
+      document.querySelector('#name')?.classList.remove('border-orange-950')
+      const nameErrorElement = document.querySelector('#name')?.nextElementSibling
+      if (nameErrorElement) {
+        nameErrorElement.textContent = 'Isi nama dulu, bit'
+      }
+    } else {
+      document.querySelector('#name')?.classList.remove('border-red-500')
+      document.querySelector('#name')?.classList.add('border-orange-950')
+      const nameErrorElement = document.querySelector('#name')?.nextElementSibling
+      if (nameErrorElement) {
+        nameErrorElement.textContent = ''
+      }
+
+      nextButton.querySelector('span')!.textContent = 'Lanjut'
+      nextButton.classList.remove('w-full')
+      nextButton.classList.add('flex-1')
+      document.querySelector('#prev-button')!.classList.remove('hidden')
+      document.querySelector('#prev-button')!.classList.add('flex')
+
+      document.querySelector('#disclaimer')?.classList.add('hidden')
+      document.querySelector('.step-1')?.classList.add('hidden')
+      document.querySelector('.step-2')?.classList.remove('hidden')
+
+      currentStep += 1
+    }
+
+    return
+  }
+
+  if (currentStep >= 2 && currentStep < 3) {
+    const selectedStop = formData.get(`selectedStops-step-${currentStep}`)
+    const isStepValid = validateCurrentStep(selectedStop)
+
+    if (isStepValid) {
+      document.querySelector(`.step-${currentStep}`)?.classList.add('hidden')
+      document.querySelector(`.step-${currentStep + 1}`)?.classList.remove('hidden')
+      currentStep += 1
+    }
+
+    return
+  }
+
+  if (currentStep === 3) {
+    const selectedStop = formData.get(`selectedStops-step-${currentStep}`)
+    const isStepValid = validateCurrentStep(selectedStop)
+
+    if (isStepValid) {
+      document.querySelector(`.step-${currentStep}`)?.classList.add('hidden')
+      document.querySelector(`.step-${currentStep + 1}`)?.classList.remove('hidden')
+      nextButton.querySelector('span')!.textContent = 'Cek'
+      currentStep += 1
+    }
+
+    return
+  }
+
+  if (currentStep === 4) {
+    nextButton.type = 'submit'
+  }
+})
+
+document.querySelector('#prev-button')?.addEventListener('click', (e) => {
+  const prevButton = e.currentTarget as HTMLButtonElement
+  const nextButton = document.querySelector<HTMLButtonElement>('#next-button')!
+
+  if (currentStep === 2) {
+    nextButton.querySelector('span')!.textContent = 'Mulai Cek Kekarbitan'
+    nextButton.classList.add('w-full')
+    nextButton.classList.remove('flex-1')
+    prevButton.classList.add('hidden')
+    prevButton.classList.remove('flex')
+
+    document.querySelector('#disclaimer')?.classList.remove('hidden')
+    document.querySelector('.step-1')?.classList.remove('hidden')
+    document.querySelector('.step-2')?.classList.add('hidden')
+
+    currentStep -= 1
+    return
+  }
+
+  if (currentStep >= 3 && currentStep < 4) {
+    document.querySelector(`.step-${currentStep - 1}`)?.classList.remove('hidden')
+    document.querySelector(`.step-${currentStep}`)?.classList.add('hidden')
+
+    currentStep -= 1
+    return
+  }
+
+  if (currentStep === 4) {
+    document.querySelector(`.step-${currentStep - 1}`)?.classList.remove('hidden')
+    document.querySelector(`.step-${currentStep}`)?.classList.add('hidden')
+    nextButton.querySelector('span')!.textContent = 'Lanjut'
+    nextButton.type = 'button'
+
+    currentStep -= 1
+    return
+  }
+})
+
+document.querySelector<HTMLInputElement>('#name')?.addEventListener('change', (e) => {
+  (e.currentTarget as HTMLInputElement)?.classList.remove('border-red-500');
+  (e.currentTarget as HTMLInputElement | null)?.classList.add('border-orange-950')
+  const nameErrorElement = document.querySelector('#name')?.nextElementSibling
+  if (nameErrorElement) {
+    nameErrorElement.textContent = ''
+  }
 })
